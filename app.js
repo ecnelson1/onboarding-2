df5453648be69c791689f4706d031ab140f647c4
Eion Nelson <ecnelson1@live.com>
2022-01-19 15:14:13 -0800

    imported people to app.js and grabbed elements in js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions
+import { people } from './data.js';
+
+// console.log('People', people);
 
 // grab DOM elements
+const selectEl = document.getElementById('name');
+const nameEl = document.querySelector('.name');
+const ageEl = document.querySelector('.age');
+const bioEl = document.querySelector('.bio');
+const hobbiesEl = document.querySelector('.hobbies');
+const container = document.querySelector('.profile');
 
-// set event listeners 
+console.log('Elements', selectEl, nameEl, ageEl, bioEl, hobbiesEl, container);
+// set event listeners
+
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
